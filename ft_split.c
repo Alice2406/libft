979a3864c4bf4550e1a13d1fd55aea3400c13914
Alice2406 <alice.niezgodzki@gmail.com>
2021-12-30 23:30:38 +0100

    meilleur strtrim ever

--- ft_split.c
+++ ft_split.c
@@ -1,19 +1,6 @@
 #include "libft.h"
-int	is_charset(char c, char *charset)
-{
-	int	i;
 
-	i = 0;
-	while (charset[i])
-	{
-		if (c == charset[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	count_words(char *str, char *charset)
+int	count_words(const char *str, char c)
 {
 	int	i;
 	int	count_words;
@@ -26,9 +13,9 @@ int	count_words(char *str, char *charset)
 		return (count_words);
 	while (str[i])
 	{
-		if (is_charset(str[i], charset))
+		if (str[i] == c)
 			f = 1;
-		if (f == 1 && !is_charset(str[i], charset))
+		if (f == 1 && str[i] != c)
 		{
 			f = 0;
 			count_words++;
@@ -38,16 +25,16 @@ int	count_words(char *str, char *charset)
 	return (count_words);
 }
 
-int	len_mot(char *str, char *charset, int i)
+int	len_mot(const char *str, char c, int i)
 {
 	int	len;
 
 	len = 0;
-	while (str[i] && is_charset(str[i], charset))
+	while (str[i] && str[i] == c)
 		i++;
 	while (str[i])
 	{
-		if (is_charset(str[i], charset))
+		if (str[i] == c)
 			return (len);
 		i++;
 		len++;
@@ -55,20 +42,20 @@ int	len_mot(char *str, char *charset, int i)
 	return (len);
 }
 
-char	**ft_split2(char **chaine, int j, char *str, char *charset)
+char	**ft_split2(char **chaine, int j, const char *str, const char c)
 {
 	int	i;
 	int	r;
 	int	len_mot_chaine;
 
 	i = 0;
-	while (j < count_words(str, charset))
+	while (j < count_words(str, c))
 	{
-		len_mot_chaine = len_mot(str, charset, i);
+		len_mot_chaine = len_mot(str, c, i);
 		chaine[j] = malloc(sizeof(char) * len_mot_chaine + 1);
 		if (chaine[j] == NULL)
 			return (NULL);
-		while (is_charset(str[i], charset))
+		while (str[i] == c)
 			i++;
 		r = 0;
 		while (r < len_mot_chaine)
@@ -84,16 +71,16 @@ char	**ft_split2(char **chaine, int j, char *str, char *charset)
 	return (chaine);
 }
 
-char	**ft_split(char *str, char *charset)
+char	**ft_split(char const *s, char c)
 {
 	char	**chaine;
 	int		j;
 
 	j = 0;
-	if (count_words(str, charset) != 0 || *str)
+	if (count_words(s, c) != 0 || *s)
 	{
-		chaine = malloc(sizeof(char *) * (count_words(str, charset) + 1));
-		chaine = ft_split2(chaine, j, str, charset);
+		chaine = malloc(sizeof(char *) * (count_words(s, c) + 1));
+		chaine = ft_split2(chaine, j, s, c);
 		return (chaine);
 	}
 	else
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,19 +1,63 @@
 #include "libft.h"
 
+static int	is_charset(char c, const char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int size_str(char const *s1, char const *set)
+{
+	int size;
+	int i;
+
+	size = 0;
+	i = 0;
+	while(s1[i])
+	{
+		if(is_charset(s1[i], set) == 0)
+			size++;
+		i++;
+	}
+	return (size);
+}
+
+static unsigned int find_start(char const *s1, char const *set)
+{
+	unsigned int start;
+	int i;
+
+	start = 0;
+	i = 0;
+	while (s1[i])
+	{
+		if(is_charset(s1[i], set) == 1)
+		{
+			start++;
+			i++;
+		}
+		else
+			return (start);
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-		size_t size;
+	char *str;
+	int size;
+	unsigned int start;
 
-		size = 0;
-		while (s1)
-		{
-			while (set)
-			{
-				if(*set == *s1)
-					s1++;
-				else if(!set)
-					i++;
-				set++;
-			}
-			s1++;
-		}
+	size = size_str(s1, set);
+	start = find_start(s1, set);
+	str = ft_substr(s1, start, size);
+	return (str);
+}	
