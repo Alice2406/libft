143f64cc4b37bc8dece9d9fce9d713b052a0cb41
Alice2406 <alice.niezgodzki@gmail.com>
2022-03-16 19:06:19 +0100

    norme

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: aniezgod <aniezgod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 16:14:19 by aniezgod          #+#    #+#             */
-/*   Updated: 2022/03/12 21:34:10 by aniezgod         ###   ########.fr       */
+/*   Updated: 2022/03/16 18:59:50 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*tmp;
 
 	tmp = (*lst);
-	if(!tmp)
+	if (!tmp)
 		(*lst) = new;
-	while(tmp)
+	while (tmp)
 	{
-		if(tmp->next == NULL)
+		if (tmp->next == NULL)
 		{
 			tmp->next = new;
 			return ;
@@ -29,4 +29,3 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		tmp = tmp->next;
 	}
 }
-
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,24 +6,24 @@
 /*   By: aniezgod <aniezgod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 16:51:01 by aniezgod          #+#    #+#             */
-/*   Updated: 2022/03/16 17:57:25 by aniezgod         ###   ########.fr       */
+/*   Updated: 2022/03/16 19:02:22 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*tmp;
 
-	if(!lst || !f || !del)
+	if (!lst || !f || !del)
 		return (0);
 	new = NULL;
-	while(lst)
+	while (lst)
 	{
 		tmp = ft_lstnew((*f)(lst->content));
-		if(!tmp)
+		if (!tmp)
 		{
 			ft_lstclear(&new, del);
 			break ;
